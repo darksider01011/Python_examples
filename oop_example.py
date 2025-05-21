fa8c5c415885a9e59fe03a77f7c379d9445d7e0c
mostafa taghizadeh <132552898+darksider01011@users.noreply.github.com>
2025-05-21 02:47:09 -0700

    Update oop_example.py

--- oop_example.py
+++ oop_example.py
@@ -1,1 +1,30 @@
+import requests
 
+class url:
+    # Class attribute
+    attr = "Class attribute"
+
+    # Magic method
+    def __init__(self, uri):
+        self.uri = uri
+
+    # Instance method
+    def status_code(self):
+        r = requests.get(self.uri)
+        print(r.status_code)
+    
+    # Instance method
+    def response(self):
+        r = requests.get(self.uri)
+        print(r.text)
+
+    # Instance method
+    def header(self):
+        r = requests.get(self.uri)
+        print(r.headers)        
+
+amazon = url("https://amazon.com")
+
+amazon.status_code()
+amazon.header()
+amazon.response()
